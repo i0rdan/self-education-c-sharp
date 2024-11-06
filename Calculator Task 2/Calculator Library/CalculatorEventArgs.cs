b9207579f85c7f526fdb0ae448176b70db1a147e
i0rdan <kazusev2000@mail.ru>
2024-11-06 20:50:05 +0300

    feat: calc library

---
+++ Calculator Task 2/Calculator Library/CalculatorEventArgs.cs
@@ -0,0 +1,18 @@
+﻿namespace Calculator_Library
+{
+    public class CalculatorEventArgs : EventArgs
+    {
+        public readonly double operand1;
+
+        public readonly double operand2;
+
+        public readonly double result;
+
+        public CalculatorEventArgs(double operand1, double operand2, double result)
+        {
+            this.operand1 = operand1;
+            this.operand2 = operand2;
+            this.result = result;
+        }
+    }
+}
