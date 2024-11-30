3ce5177cc4e82e2f1e0149442b5aef5abb0a4834
i0rdan <kazusev2000@mail.ru>
2024-11-30 16:26:42 +0300

    feat(analizer): program

---
+++ Brackets Analysis Task 4/Brackets Analysis Program/Program.cs
@@ -0,0 +1,25 @@
+﻿using Analizer_Library;
+
+while (true)
+{
+    try
+    {
+        Console.WriteLine("Please enter your string for validation:");
+
+        string? s = Console.ReadLine();
+        if(BracketsAnalizer.IsValid(s))
+        {
+            Console.WriteLine("String is valid!");
+        }
+        else
+        {
+            Console.WriteLine("String is not valid!");
+        }
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+
+    Console.WriteLine();
+}
