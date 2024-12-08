84151d7dfea3de4adc83b7128f9f6b66df7d10ba
i0rdan <kazusev2000@mail.ru>
2024-12-08 16:19:49 +0300

    feat: program

---
+++ MyStringBuilder Task 6/Program/Program.cs
@@ -0,0 +1,18 @@
+﻿using MyStringBuilder_Library;
+
+var s = new MyStringBuilder("test");
+s.Append("1");
+s.Append("2");
+s.AppendLine();
+s.Append("3");
+
+Console.WriteLine(s);
+
+s.Undo();
+
+Console.WriteLine(s);
+
+s.Undo();
+s.Undo();
+
+Console.WriteLine(s);
