bc0f3ea068f39636354bea62d2b6c916a951a6f2
i0rdan <kazusev2000@mail.ru>
2025-07-18 12:23:41 +0300

    feat(codewars-task-5): init

---
+++ CodeWars Tasks/CodeWars Task 5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+public static class Kata5
+{
+    public static readonly Dictionary<char, char> dnaDict = new()
+    {
+        { 'T', 'A' },
+        { 'A', 'T' },
+        { 'C', 'G' },
+        { 'G', 'C' },
+    };
+
+    public static string MakeComplement(string dna)
+    {
+        var sb = new StringBuilder();
+
+        foreach (var c in dna)
+        {
+            sb.Append(dnaDict[c]);
+        }
+
+        return sb.ToString();
+    }
+}
