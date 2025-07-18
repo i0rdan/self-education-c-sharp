d300a450336576bc02891e0a0bc4edba3de57648
i0rdan <kazusev2000@mail.ru>
2025-07-18 12:08:21 +0300

    feat(codewars-task-2): init

---
+++ CodeWars Tasks/CodeWars Task 2/Program.cs
@@ -0,0 +1,24 @@
+﻿public static class Kata2
+{
+    public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+    {
+        var listWithUniqueOrderItems = new List<T>();
+
+        foreach (var item in iterable)
+        {
+            if (listWithUniqueOrderItems.Count == 0)
+            {
+                listWithUniqueOrderItems.Add(item);
+
+                continue;
+            }
+
+            if (!listWithUniqueOrderItems.Last()!.Equals(item))
+            {
+                listWithUniqueOrderItems.Add(item);
+            }
+        }
+
+        return listWithUniqueOrderItems;
+    }
+}
