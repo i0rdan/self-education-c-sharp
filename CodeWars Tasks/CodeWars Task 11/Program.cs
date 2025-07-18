e43d467d9ef702af2515958b42079db532e8fa75
i0rdan <kazusev2000@mail.ru>
2025-07-18 12:41:24 +0300

    feat(codewars-task-11): init

---
+++ CodeWars Tasks/CodeWars Task 11/Program.cs
@@ -0,0 +1,6 @@
+﻿public static class Kata11
+{
+    public static string SpinWords(string sentence) => sentence.Length > 0
+        ? string.Join(' ', sentence.Split(' ').Select((elem) => elem.Length >= 5 ? string.Join(string.Empty, elem.Reverse()) : elem))
+        : string.Empty;
+}
