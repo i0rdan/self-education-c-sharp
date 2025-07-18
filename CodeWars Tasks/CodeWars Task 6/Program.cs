8c6f1807ba056fd1a84b06cfdc7032368f87f9b7
i0rdan <kazusev2000@mail.ru>
2025-07-18 12:27:28 +0300

    feat(codewars-task-6): init

---
+++ CodeWars Tasks/CodeWars Task 6/Program.cs
@@ -0,0 +1,16 @@
+﻿public static class Kata6
+{
+    public static bool XO(string input)
+    {
+        var xCount = 0;
+        var oCount = 0;
+
+        foreach (var c in input.ToLower())
+        {
+            if (c == 'x') xCount++;
+            if (c == 'o') oCount++;
+        }
+
+        return xCount == oCount;
+    }
+}
