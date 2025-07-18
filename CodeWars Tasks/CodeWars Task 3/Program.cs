de97ceda907670f63cded5a6a63ed37fab426954
i0rdan <kazusev2000@mail.ru>
2025-07-18 12:15:40 +0300

    feat(codewars-task-3): init

---
+++ CodeWars Tasks/CodeWars Task 3/Program.cs
@@ -0,0 +1,4 @@
+﻿public static class Kata3
+{
+    public static string GetMiddleSymbolOfString(string s) => s.Substring(s.Length % 2 == 1 ? s.Length / 2 : s.Length / 2 - 1, s.Length % 2 == 1 ? 1 : 2);
+}
