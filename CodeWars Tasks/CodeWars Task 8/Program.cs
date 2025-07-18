59919aaecf041d65f43d2aa5387a9786d7520175
i0rdan <kazusev2000@mail.ru>
2025-07-18 12:33:25 +0300

    feat(codewars-task-8): init

---
+++ CodeWars Tasks/CodeWars Task 8/Program.cs
@@ -0,0 +1,23 @@
+﻿public static class Kata8
+{
+    public static int Solution(int value)
+    {
+        if (value < 3)
+        {
+            return 0;
+        }
+
+        var sum = 0;
+
+        for (var i = 3; i < value; i += 1)
+        {
+            if (i % 3 == 0 || i % 5 == 0)
+            {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+
+}
