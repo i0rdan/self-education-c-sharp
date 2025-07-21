8f0745435519f7cf572d1ae7b15a3532df39303f
i0rdan <kazusev2000@mail.ru>
2025-07-21 17:21:26 +0300

    feat(codewars-task-12): init

---
+++ CodeWars Tasks/CodeWars Task 12/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+public static class Kata12
+{
+    public static string PigIt(string str) => string.Join(" ", str.Split(' ')
+        .Select(word =>
+        Regex.IsMatch(word, @"^[A-Za-z]+$")
+            ? word[1..] + word[0] + "ay"
+            : word
+        ));
+}
